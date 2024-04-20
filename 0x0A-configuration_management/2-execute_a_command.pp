5cd19a6f2bd78c37f8c171ffdd08e128f02714af
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-04-19 20:27:01 -0700

    Execute a command

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#This code kills a process && works together with the killmenow file which has already been provided
+exec { killmenow':
+  command  =>   '/usr/bin/pkill killmenow',
+  provider =>   'shell',
+  returns  =>   [0, 1],
+}
