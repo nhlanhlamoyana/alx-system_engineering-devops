a6d719881db1e4241351e43773e48b3c4328d621
root <moyananhlanhla27@gmail.com>
2024-04-24 16:06:54 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 #This code kills a process && works together with the killmenow file which has already been provided
-exec { killmenow':
-  command  =>   '/usr/bin/pkill killmenow',
-  provider =>   'shell',
-  returns  =>   [0, 1],
+exec { 'pkill':
+  command  => 'pkill -9 -f killmenow',
+  path     => ['/usr/bin', '/usr/sbin', '/bin']
 }
